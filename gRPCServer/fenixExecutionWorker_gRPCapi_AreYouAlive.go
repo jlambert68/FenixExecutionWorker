4bacab9419df8ae5a1bcd9334b3b3cc4542b0794
jlamber <jonas.lambert@drakon.se>
2022-09-26 20:32:33 +0200

    Generated base for gPRC regarding 'ProcessTestInstructionExecution'

--- gRPCServer/fenixExecutionWorker_gRPCapi_AreYouAlive.go
+++ gRPCServer/fenixExecutionWorker_gRPCapi_AreYouAlive.go
@@ -1,6 +1,7 @@
 package gRPCServer
 
 import (
+	"FenixExecutionWorker/common_config"
 	//"FenixExecutionWorker"
 	fenixExecutionWorkerGrpcApi "github.com/jlambert68/FenixGrpcApi/FenixExecutionServer/fenixExecutionWorkerGrpcApi/go_grpc_api"
 	"github.com/sirupsen/logrus"
@@ -20,4 +21,66 @@ func (s *fenixExecutionWorkerGrpcServicesServer) AreYouAlive(ctx context.Context
 	}).Debug("Outgoing 'gRPCServer - AreYouAlive'")
 
 	return &fenixExecutionWorkerGrpcApi.AckNackResponse{AckNack: true, Comments: "I'am alive."}, nil
+}
+
+// ReportProcessingCapability
+// Ask Client to inform Execution Server of Clients capability to execute requests in parallell, serial or no processing at all
+func (s *fenixExecutionWorkerGrpcServicesServer) ReportProcessingCapability(ctx context.Context, emptyParameter *fenixExecutionWorkerGrpcApi.EmptyParameter) (*fenixExecutionWorkerGrpcApi.AckNackResponse, error) {
+
+	s.logger.WithFields(logrus.Fields{
+		"id": "37bc2356-33a2-4e2c-9420-122df581d757",
+	}).Debug("Incoming 'gRPCServer - ReportProcessingCapability'")
+
+	s.logger.WithFields(logrus.Fields{
+		"id": "f3fd3e50-5770-48ad-8524-85f34d28545e",
+	}).Debug("Outgoing 'gRPCServer - ReportProcessingCapability'")
+
+	return &fenixExecutionWorkerGrpcApi.AckNackResponse{AckNack: true, Comments: "I'am alive."}, nil
+
+}
+
+// ProcessTestInstructionExecution
+// Fenix Execution Server send a request to Execution Worker to initiate a execution of a TestInstruction
+func (s *fenixExecutionWorkerGrpcServicesServer) ProcessTestInstructionExecution(ctx context.Context, processTestInstructionExecutionRequest *fenixExecutionWorkerGrpcApi.ProcessTestInstructionExecutionRequest) (processTestInstructionExecutionResponse *fenixExecutionWorkerGrpcApi.ProcessTestInstructionExecutionResponse, err error) {
+
+	s.logger.WithFields(logrus.Fields{
+		"id": "37bc2356-33a2-4e2c-9420-122df581d757",
+	}).Debug("Incoming 'gRPCServer - ReportProcessingCapability'")
+
+	defer s.logger.WithFields(logrus.Fields{
+		"id": "f3fd3e50-5770-48ad-8524-85f34d28545e",
+	}).Debug("Outgoing 'gRPCServer - ReportProcessingCapability'")
+
+	userId := "Execution Server"
+
+	// Check if Client is using correct proto files version
+	returnMessage := common_config.IsClientUsingCorrectTestDataProtoFileVersion(userId, fenixExecutionWorkerGrpcApi.CurrentFenixExecutionServerProtoFileVersionEnum(processTestInstructionExecutionRequest.ProtoFileVersionUsedByClient))
+	if returnMessage != nil {
+
+		// Exiting
+		processTestInstructionExecutionResponse = &fenixExecutionWorkerGrpcApi.ProcessTestInstructionExecutionResponse{
+			AckNackResponse:                returnMessage,
+			TestInstructionExecutionUuid:   "",
+			ExpectedExecutionDuration:      nil,
+			TestInstructionCanBeReExecuted: false,
+		}
+
+		return processTestInstructionExecutionResponse, nil
+	}
+
+	// Generate response
+	processTestInstructionExecutionResponse = &fenixExecutionWorkerGrpcApi.ProcessTestInstructionExecutionResponse{
+		AckNackResponse: &fenixExecutionWorkerGrpcApi.AckNackResponse{
+			AckNack:                      true,
+			Comments:                     "",
+			ErrorCodes:                   nil,
+			ProtoFileVersionUsedByClient: fenixExecutionWorkerGrpcApi.CurrentFenixExecutionServerProtoFileVersionEnum(common_config.GetHighestFenixTestDataProtoFileVersion()),
+		},
+		TestInstructionExecutionUuid:   "",
+		ExpectedExecutionDuration:      nil,
+		TestInstructionCanBeReExecuted: false,
+	}
+
+	return processTestInstructionExecutionResponse, nil
+
 }
