285caf218fe0f4f435681c601a05165e4b43b6e9
jlambert68 <jonas.lambert@drakon.se>
2024-02-01 11:17:11 +0100

    New version of access token in test function to sign a message

--- signTest.go
+++ signTest.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"FenixExecutionWorker/common_config"
-	"FenixExecutionWorker/gcp"
 	iam_credentials "cloud.google.com/go/iam/credentials/apiv1"
 	"context"
 	"crypto/tls"
+	"fmt"
 	fenixSyncShared "github.com/jlambert68/FenixSyncShared"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	iam_credentialspb "google.golang.org/genproto/googleapis/iam/credentials/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	"log"
 )
 
@@ -66,12 +68,8 @@ func signTest(
 		Payload: data,
 	}
 
-	var (
-		returnAckNack bool
-		returnMessage string
-	)
-	ctx, returnAckNack, returnMessage = gcp.Gcp.GenerateGCPAccessToken(ctx, gcp.GenerateTokeForIamCredentials)
-	if returnAckNack == false || len(returnMessage) > 0 {
+	ctx, err = attachCredentials(ctx)
+	if err != nil {
 		common_config.Logger.WithFields(logrus.Fields{
 			"ID":  "1e46ea03-6a67-4ee1-853d-408d60b440d5",
 			"err": err,
@@ -99,4 +97,31 @@ func signTest(
 
 	// Return result
 	return signedMessage, hashOfSignature, hashedKeyId, err
+}
+
+func attachCredentials(ctx context.Context) (context.Context, error) {
+	// Get default credentials (this works in GCP environments like GCE, GKE, Cloud Run, etc.)
+	creds, err := google.FindDefaultCredentials(ctx, iam_credentials.DefaultAuthScopes()...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use the TokenSource from the credentials
+	tokenSource := creds.TokenSource
+
+	// Retrieve an OAuth 2.0 token
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the token is valid
+	if !token.Valid() {
+		return nil, fmt.Errorf("token is invalid")
+	}
+
+	// Create a new context with the token attached
+	newCtx := metadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token.AccessToken)
+
+	return newCtx, nil
 }
