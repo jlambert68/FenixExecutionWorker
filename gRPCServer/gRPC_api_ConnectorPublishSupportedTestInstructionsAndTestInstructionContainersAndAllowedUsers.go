92d22d1683b1504d2ad3e5c6f3a451da250d4963
jlambert68 <jonas.lambert@drakon.se>
2024-10-23 15:26:25 +0200

    Updated to support new version for signing senders message

--- gRPCServer/gRPC_api_ConnectorPublishSupportedTestInstructionsAndTestInstructionContainersAndAllowedUsers.go
+++ gRPCServer/gRPC_api_ConnectorPublishSupportedTestInstructionsAndTestInstructionContainersAndAllowedUsers.go
@@ -155,84 +155,16 @@ func (s *fenixExecutionWorkerConnectorGrpcServicesServer) ConnectorPublishSuppor
 	supportedTestInstructionsAndTestInstructionContainersAndAllowedUsersGrpcBuilderMessage, err = shared_code.
 		GenerateTestInstructionAndTestInstructionContainerAndUserGrpcBuilderMessage(
 			supportedTestInstructionsAndTestInstructionContainersAndAllowedUsersGrpcWorkerMessage.GetConnectorDomain().GetConnectorsDomainUUID(),
-			testInstructionsAndTestInstructionContainersFromGrpcBuilderMessage)
+			testInstructionsAndTestInstructionContainersFromGrpcBuilderMessage,
+			supportedTestInstructionsAndTestInstructionContainersAndAllowedUsersGrpcWorkerMessage.MessageSignatureData)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Get Message to sign to prove identity
-	succeededToSend, responseMessage, messageToSign := fenixGuiBuilderObject.
-		SendGetMessageToSignToProveCallerIdentity()
-
-	if succeededToSend == false {
-		s.logger.WithFields(logrus.Fields{
-			"id":              "7fb5a94a-7984-47f8-8c08-0d7da1446b94",
-			"responseMessage": responseMessage,
-		}).Error("Got some error when sending 'GetMessageToSignToProveCallerIdentity'")
-
-		// Generate response
-		returnMessage = &fenixExecutionWorkerGrpcApi.AckNackResponse{
-			AckNack:                      succeededToSend,
-			Comments:                     fmt.Sprintf("Messagage from BuilderServer: '%s'", responseMessage),
-			ErrorCodes:                   nil,
-			ProtoFileVersionUsedByClient: fenixExecutionWorkerGrpcApi.CurrentFenixExecutionWorkerProtoFileVersionEnum(common_config.GetHighestExecutionWorkerProtoFileVersion()),
-		}
-
-		return returnMessage, nil
-
-	}
-
-	// Specify the service account to be used when signing
-	var serviceAccountUsedWhenSigning string
-	serviceAccountUsedWhenSigning = fmt.Sprintf("projects/-/serviceAccounts/%s",
-		common_config.ServiceAccountUsedForSigningMessage)
-
-	// Sign Message to prove Identity to BuilderServer
-	var hashOfSignature string
-	var hashedKeyId string
-	if common_config.ExecutionLocationForWorker == common_config.GCP {
-
-		hashOfSignature, hashedKeyId, err = shared_code.SignMessageToProveIdentityToBuilderServer(
-			messageToSign,
-			serviceAccountUsedWhenSigning,
-			true)
-
-	} else {
-
-		hashOfSignature, hashedKeyId, err = shared_code.SignMessageToProveIdentityToBuilderServer(
-			messageToSign,
-			serviceAccountUsedWhenSigning,
-			false)
-	}
-
-	if err != nil {
-		s.logger.WithFields(logrus.Fields{
-			"id":  "bb8284b2-cc4f-440f-8474-a2ea12adceb1",
-			"err": err,
-		}).Error("Got some error when signing message")
-
-		returnMessage = &fenixExecutionWorkerGrpcApi.AckNackResponse{
-			AckNack:                      false,
-			Comments:                     fmt.Sprintf("Got some error when signing message. '%s'", err.Error()),
-			ErrorCodes:                   nil,
-			ProtoFileVersionUsedByClient: fenixExecutionWorkerGrpcApi.CurrentFenixExecutionWorkerProtoFileVersionEnum(common_config.GetHighestExecutionWorkerProtoFileVersion()),
-		}
-
-		return returnMessage, err
-	}
-
-	// Add Signed message
-	var signedMessageByWorkerServiceAccountMessage *fenixTestCaseBuilderServerGrpcApi.SignedMessageByWorkerServiceAccountMessage
-	signedMessageByWorkerServiceAccountMessage = &fenixTestCaseBuilderServerGrpcApi.SignedMessageByWorkerServiceAccountMessage{
-		MessageToBeSigned: messageToSign,
-		HashOfSignature:   hashOfSignature,
-		HashedKeyId:       hashedKeyId,
-	}
-	supportedTestInstructionsAndTestInstructionContainersAndAllowedUsersGrpcBuilderMessage.
-		SignedMessageByWorkerServiceAccount = signedMessageByWorkerServiceAccountMessage
-
 	// Publish Supported TestInstructions, TestInstructionContainers And AllowedUsers To FenixGuiBuilderServer
+	var succeededToSend bool
+	var responseMessage string
 	succeededToSend, responseMessage = fenixGuiBuilderObject.
 		SendPublishSupportedTestInstructionsAndTestInstructionContainersAndAllowedUsersToFenixGuiBuilderServer(
 			supportedTestInstructionsAndTestInstructionContainersAndAllowedUsersGrpcBuilderMessage)
